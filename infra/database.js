e55d89f6e837859d34c52e3edc0d6d5936a3d348
Ramonmelod <ramonmelo.com@gmail.com>
2025-02-27 09:44:42 -0300

    feat: change from string connection to config object connection in database.js

--- infra/database.js
+++ infra/database.js
@@ -3,16 +3,32 @@ const sql = require("mssql");
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env.development" }); // here is used the .env.development file, but the production shel use the .env file
 
-const database = async (queryObject) => {
+const sqlConfig = {
+  user: process.env.SQLSERVERUSER,
+  password: process.env.SQLSERVERPASSWORD,
+  database: process.env.SQLSERVERDATABASE,
+  server: process.env.HOST,
+  port: 1400,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: false, // for azure
+    trustServerCertificate: false, // change to true for local dev / self-signed certs
+  },
+};
+
+async function database(queryObject) {
   try {
-    await sql.connect(
-      `Server=${process.env.HOST},1400;Database=${process.env.SQLSERVERDATABASE};User Id=${process.env.SQLSERVERUSER};Password=${process.env.SQLSERVERPASSWORD};Encrypt=false`
-    );
+    // make sure that any items are correctly URL encoded in the connection string
+    await sql.connect(sqlConfig);
     const result = await sql.query(queryObject);
     return result;
   } catch (error) {
     console.log(error);
   }
-};
+}
 
 module.exports = { database };
