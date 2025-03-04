7fd93d17c2d9f2fdfcddd135ba474d601e16a136
Ramonmelod <ramonmelo.com@gmail.com>
2025-03-04 16:28:10 -0300

    feat: implementation of the `OsInformations` component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,13 +8,33 @@ async function fetchApi(key) {
 }
 
 const Dashboard = () => {
-  const response = useSWR("/api/v1/os_status", fetchApi); //endpoint access without localhost text allowed by proxy property in package json
   return (
     <>
       <h1>Seta Dashboard</h1>
-      <pre>{JSON.stringify(response.data, null, 2)}</pre>
+      <OsInformations />
     </>
   );
 };
+
+function OsInformations() {
+  const { data, isLoading } = useSWR("/api/v1/os_status", fetchApi); //endpoint access without localhost text allowed by proxy property in package json
+  let servicoNumeroText = "carregando...";
+  let openDate = "carregando...";
+  let licensePlate = "carregando...";
+  if (!isLoading && data) {
+    servicoNumeroText = data.recordsets[0][0].ServicoNumero;
+    openDate = data.recordsets[0][0].AberturaDataHora;
+    licensePlate = data.recordsets[0][0].Placa;
+  }
+  return (
+    <>
+      <div>
+        Número Ordem de serviço: {JSON.stringify(servicoNumeroText, null, 2)}
+      </div>
+      <div>Data de abertura: {JSON.stringify(openDate, null, 2)}</div>
+      <div>Placa: {JSON.stringify(licensePlate, null, 2)}</div>
+    </>
+  );
+}
 
 export default Dashboard;
