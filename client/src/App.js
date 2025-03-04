ab427e4f19e768281c17d74c137cec2b75e8024a
Ramonmelod <ramonmelo.com@gmail.com>
2025-03-04 11:13:41 -0300

    feat: initial implementation of the api query

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Routes from "./routes";
 
+async function query() {
+  const response = await fetch("http://localhost:8080/api/v1/os_status");
+  const data = response.json();
+  return data;
+}
+
 export default function App() {
-  return <Routes />;
+  console.log(query());
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
