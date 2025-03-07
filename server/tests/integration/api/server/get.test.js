be42a6e17e548b90bfb8c7f4b3bd155f9731ef82
Ramonmelod <ramonmelo.com@gmail.com>
2025-03-07 15:23:41 -0300

    test: update the test access endpoint and port

--- server/tests/integration/api/server/get.test.js
+++ server/tests/integration/api/server/get.test.js
@@ -1,7 +1,7 @@
 const { json } = require("express");
 
 test("teste status response é 200", async () => {
-  const response = await fetch("http://localhost:3000/");
+  const response = await fetch("http://localhost:8080/api/v1/os_status");
 
   expect(response.status).toBe(200);
 });
