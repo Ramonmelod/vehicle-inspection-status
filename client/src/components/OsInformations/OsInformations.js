2ea048c24c07a29c2591a5d682345093a12140ab
Ramonmelod <ramonmelo.com@gmail.com>
2025-03-10 15:42:40 -0300

    feat: implement the message that says that the service is not available

--- client/src/components/OsInformations/OsInformations.js
+++ client/src/components/OsInformations/OsInformations.js
@@ -2,14 +2,25 @@ import useSWR from "swr";
 import "./OsInformations.css";
 
 async function fetchApi(key) {
-  const response = await fetch(key);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(key);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function OsInformations() {
   const { data, isLoading } = useSWR("/api/v1/os_status", fetchApi);
   const padding = "--";
+  if (isLoading) return <p>Carregando informações...</p>;
+  if (!data)
+    return (
+      <p style={{ textAlign: "center", fontWeight: "bold", color: "#b21418" }}>
+        Serviço indisponível. Tente novamente mais tarde.
+      </p>
+    );
 
   return (
     <>
@@ -28,40 +39,46 @@ export default function OsInformations() {
             </tr>
           </thead>
           <tbody>
-            {data.map((os, index) => (
-              <tr key={index}>
-                <td>{os.serviceNumber || padding}</td>
-                <td>{os.licensePlate || padding}</td>
-                <td>
-                  {(() =>
-                    os.openDate
-                      ? new Date(os.openDate).toLocaleTimeString("pt-BR")
-                      : padding)()}
-                </td>
-                <td>
-                  {(() =>
-                    os.finalization
-                      ? new Date(os.finalization).toLocaleTimeString("pt-BR")
-                      : padding)()}
-                </td>
-                <td>{os.TipoCsvSerproNome || padding}</td>
-                <td>
-                  {" "}
-                  {(() => {
-                    switch (os.status) {
-                      case 0:
-                        return "Aberto";
-                      case 1:
-                        return "Finalizado";
-                      case 2:
-                        return "Cancelado";
-                      default:
-                        return padding;
-                    }
-                  })()}
-                </td>
-              </tr>
-            ))}
+            {(() =>
+              data?.map(
+                // the ? in data make the variable data to be ignored if it is undefined
+                (os, index) => (
+                  <tr key={index}>
+                    <td>{os.serviceNumber || padding}</td>
+                    <td>{os.licensePlate || padding}</td>
+                    <td>
+                      {(() =>
+                        os.openDate
+                          ? new Date(os.openDate).toLocaleTimeString("pt-BR")
+                          : padding)()}
+                    </td>
+                    <td>
+                      {(() =>
+                        os.finalization
+                          ? new Date(os.finalization).toLocaleTimeString(
+                              "pt-BR"
+                            )
+                          : padding)()}
+                    </td>
+                    <td>{os.TipoCsvSerproNome || padding}</td>
+                    <td>
+                      {" "}
+                      {(() => {
+                        switch (os.status) {
+                          case 0:
+                            return "Aberto";
+                          case 1:
+                            return "Finalizado";
+                          case 2:
+                            return "Cancelado";
+                          default:
+                            return padding;
+                        }
+                      })()}
+                    </td>
+                  </tr>
+                )
+              ))()}
           </tbody>
         </table>
       )}
