2089564da8944157e628b3bb992855e1b5c4ee0e
Ramonmelod <ramonmelo.com@gmail.com>
2025-03-13 09:32:29 -0300

    style: change the position of the logo and the title in dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,16 +5,38 @@ import OsInformations from "../components/OsInformations/OsInformations.js";
 const Dashboard = () => {
   return (
     <>
-      <h1 className="title">
-        <span style={{ color: "black" }}>Seta</span>{" "}
-        <span style={{ color: "#b21418" }}>Teresina</span>
-      </h1>
       <img
         src="logo-grupo-seta.jpg"
         alt="Logo"
         width="200"
-        style={{ position: "relative", left: "15%", top: "15%" }}
+        style={{ position: "relative", left: "12%", top: "12vh" }}
       />
+      <h1
+        className="title"
+        style={{
+          color: "black",
+          position: "relative",
+          left: "42%",
+          top: "8vh",
+        }}
+      >
+        <span
+          style={{
+            color: "black",
+            position: "relative",
+          }}
+        >
+          Seta
+        </span>{" "}
+        <span
+          style={{
+            color: "#b21418",
+            position: "relative",
+          }}
+        >
+          Teresina
+        </span>
+      </h1>
       <OsInformations />
     </>
   );
